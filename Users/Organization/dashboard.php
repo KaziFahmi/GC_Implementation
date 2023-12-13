106d570958f697ef06421223ca7db463c5343307
KaziFahmi <gildarts189@gmail.com>
2023-12-14 03:27:19 +0600

    Added the login and registration functionality for all types of users.

---
+++ Users/Organization/dashboard.php
@@ -0,0 +1,6 @@
+<?php
+    require_once 'helpers.php';
+    isLoggedIn();
+    echo "<h1>Organization Dashboard</h1>";
+
+?>
--- test.php
+++ test.php
@@ -3,29 +3,9 @@ require_once "sql/sqlhelpers.php";
 
 function main(){
     
-    $blockchain_table = new Table("blockchain", "number", "hash", "previous", "data", "nonce");
-    // $blockchain_table->insert("1", "1234", "0000", "Block 1 Data", "1234");
-    // $blockchain_table->insert("2", "2345", "1234", "Block 2 Data", "1234");
-    // $blockchain_table->insert("3", "3456", "2345", "Block 3 Data", "1234");
-    // $blockchain_table->insert("4", "4567", "3456", "Block 4 Data", "1234");
-
-    print($blockchain_table->getOne("number", "3")["data"]);
-
-    // print("Before Delete One <br>");
-    // $all_data = $blockchain_table->getAll();
-    // foreach($all_data as $row){
-    //     print($row['data']);
-    //     print("<br>");
-    // }
-
-    // $blockchain_table->deleteOne("number", "2");
-
-    // print("After Delete One <br>");
-    // $all_data = $blockchain_table->getAll();
-    // foreach($all_data as $row){
-    //     print($row['data']);
-    //     print("<br>");
-    // }
+    $donors = new Table("donors", "name", "email", "username", "password"); 
+    $organization = new Table("organization", "name", "email", "username", "password", "donations_received"); 
+    $workers = new Table("workers", "name", "email", "username", "password", "incentive_received"); 
 
     
 }
